7d9fffcd639e88bf786bf88320943659e5b172b1
Dora Noda <dev@stargately.com>
2023-01-06 18:43:58 -0800

    better sitemap generation fix https://github.com/BlockEdenHQ/pm/issues/59

--- scripts/sitemap/generate-sitemap.js
+++ scripts/sitemap/generate-sitemap.js
@@ -1,20 +1,42 @@
 const sm = require('sitemap');
-const fs = require('fs');
+var XmlSitemap = require('xml-sitemap');
+var xmlString = require('fs').readFileSync(`${__dirname}/../../build/sitemap.xml`);
+var oldSitemap = new XmlSitemap(xmlString);
 
-const urls = [
-  '/dash/login/',
-  '/dash/sign-up/',
-  '/analytics/public/dashboard/8aebd278-8f33-43ea-95f3-8baf3ecab5cf/',
-  '/sui-health-checker/',
-].map(it => ({
-  url: it,
-  changefreq: 'daily',
-  priority: 0.5,
-}));
+[
+  // external
+  ...[
+    'https://blockeden.xyz/analytics/public/dashboard/8aebd278-8f33-43ea-95f3-8baf3ecab5cf/',
+    'https://blockeden.xyz/sui-health-checker/',
+  ].map(it => ({
+    url: it,
+    changefreq: 'weekly',
+    priority: 0.5,
+  })),
 
-const it = sm.createSitemap({
-  hostname: 'https://blockeden.xyz',
-  urls,
+  // high priority
+  ...[
+    'https://blockeden.xyz/about-us/',
+    'https://blockeden.xyz/dash/login/',
+    'https://blockeden.xyz/dash/sign-up/',
+    'https://blockeden.xyz/pricing/',
+    'https://blockeden.xyz/docs/intro/',
+    'https://blockeden.xyz/'
+  ].map(it => ({
+    url: it,
+    changefreq: 'weekly',
+    priority: 1.0,
+  })),
+].forEach(it => {
+  if (oldSitemap.hasUrl(it.url)) {
+    const {url, ...opts} = it;
+    oldSitemap.setOptionValues(it.url, opts);
+  } else {
+    oldSitemap.add(it)
+  }
 });
 
-fs.writeFileSync(`${__dirname}/sitemap.xml`, it.toString());
+oldSitemap.removeOption('lastmod');
+
+require('fs').writeFileSync(`${__dirname}/../../build/sitemap-manual.xml`, oldSitemap.xml)
+require('fs').writeFileSync(`${__dirname}/../../build/sitemap.xml`, oldSitemap.xml)
